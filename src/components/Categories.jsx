3b1f29b1e8e3900f1f083ee59f59d6c7870132a4
unknown <shashi902794@gmail.com>
2025-07-17 18:53:17 +0530

    i

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -10,7 +10,7 @@ const Categories = () => {
             <p className="text-2xl md:text-3xl font-meduim">
                 Categories
             </p>
-            <div className="grid place-items-center grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 md:gap-6 mt-6">
+            <div className="grid place-items-center grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-3 md:gap-6 mt-6">
                 {categories?.map((category, index) => (
                     <div onClick={()=> {navigate(`/products/${category.path.toLowerCase()}`); scrollTo(0,0)}} style={{backgroundColor: category.bgColor}} key={index} className="group cursor-pointer py-5 px-3 gap-2 rounded-lg flex flex-col justify-center items-center">
                         <img  src={category.image} alt="" className="group-hover:scale-108 transition max-w-28" />

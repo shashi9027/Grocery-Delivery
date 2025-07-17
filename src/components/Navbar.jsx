d15648f4742cbab762aa6b07c493e10c07279a17
unknown <shashi902794@gmail.com>
2025-07-17 14:47:03 +0530

    mobile responsive issue fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -87,7 +87,7 @@ const Navbar = () => {
 
             {/* Mobile Menu */}
             {open && 
-            <div className={`${open ? 'flex' : 'hidden'} absolute top-[60px] left-0 w-full bg-white shadow-md py-4 flex-col items-start gap-2 px-5 text-sm md:hidden`}>
+            <div className={`${open ? 'flex' : 'hidden'} absolute top-[60px] z-50 left-0 w-full bg-white shadow-md py-4 flex-col items-start gap-2 px-5 text-sm md:hidden`}>
                 <NavLink to="/" onClick={()=>  setOpen(false)}>Home</NavLink>
                 <NavLink to="/products" onClick={()=>  setOpen(false)}>All Product</NavLink>
                 {user && <NavLink to="/" onClick={()=>  setOpen(false)}>My Orders</NavLink>}
